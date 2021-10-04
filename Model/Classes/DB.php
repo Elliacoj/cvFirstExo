81aaa8c4415cc5f11e72d0b9f089b96bc48b13b9
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-10-04 15:41:34 +0200

    Db instance check and database finish

---
+++ Model/Classes/DB.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model\Classes;
+
+use PDO;
+use PDOException;
+
+class DB {
+    private string $dbName = "exoPhp";
+    private string $host = "localhost";
+    private string $userName = "root";
+    private string $password = "";
+    private static ?PDO $dbInstance = null;
+
+    public function __construct() {
+        try {
+            self::$dbInstance = new PDO("mysql:dbName=$this->dbName;host=$this->host;chartset=utf8",$this->userName, $this->password);
+            self::$dbInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$dbInstance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception) {
+            echo $exception->getMessage();
+        }
+    }
+
+    public function getInstance():PDO {
+        if(self::$dbInstance = null) {
+            new self();
+        }
+        return self::$dbInstance;
+    }
+}
